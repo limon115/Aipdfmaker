550e8caeb9451d03e20c4545d35b0ff7feecd7cb
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-06 21:03:28 +0600

    Fix: Apply missing Kotlin serialization plugin to fix cloud build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesS
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
