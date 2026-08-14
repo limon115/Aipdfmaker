9377ec584457d3a4f27a4163d4f66e4f2f0ff86a
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-14 12:40:42 +0600

    fix(gradle): dynamically sync kotlin serialization plugin version with core compiler to resolve AbstractMethodError

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,5 @@ plugins {
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google.services) apply false
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
 }
