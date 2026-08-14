51250f05989b7325df6a2d200b8de5b876e7ad5c
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-14 15:33:46 +0600

    fix(gradle): unify kotlin serialization plugin via TOML aliases to perfectly sync compiler versions and prevent AbstractMethodError

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
 
 plugins {
-    kotlin("plugin.serialization")
+    alias(libs.plugins.kotlin.serialization)
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
