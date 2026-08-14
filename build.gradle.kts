c0b96ae7cbcdcb1a62d6f5dfe66f6cb0c849a646
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-14 21:02:45 +0600

    fix(gradle): overwrite mutilated root build.gradle.kts with pristine state and safe resolution strategy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
     configurations.classpath {
         resolutionStrategy.eachDependency {
+            // ONLY force the core Kotlin compiler tools, completely ignoring kotlinx (like coroutines/serialization)
             if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin-")) {
                 useVersion("2.2.10")
             }
@@ -8,18 +11,12 @@ buildscript {
     }
 }
 
-
-        }
-    }
-}
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.google.services) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
 }
