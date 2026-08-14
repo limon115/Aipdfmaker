e0d046d99302d1e1b9795c7c69c4d42b18f5e32d
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-14 17:04:18 +0600

    fix(gradle): inject nuclear resolutionStrategy to crush transitive kotlin version leaks and flush poisoned cloud cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    configurations.classpath {
+        resolutionStrategy.eachDependency {
+            if (requested.group.startsWith("org.jetbrains.kotlin")) {
+                useVersion("2.2.10")
+            }
+        }
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
