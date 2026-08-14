9c63b9af69e385d1df97ad5172c21e4412dd29f6
limon115 <Khalid.hasan.limon2006@gmail.com>
2026-08-15 00:18:02 +0600

    fix(deps): downgrade ksp to 2.2.10-2.0.2 to match kotlin 2.2.10 compiler and resolve AbstractMethodError

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    configurations.classpath {
-        resolutionStrategy.eachDependency {
-            // ONLY force the core Kotlin compiler tools, completely ignoring kotlinx (like coroutines/serialization)
-            if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin-")) {
-                useVersion("2.2.10")
-            }
-        }
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
@@ -18,5 +7,5 @@ plugins {
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.google.services) apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
